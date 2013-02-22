b9713114689e6a54be27e7c59f6fec283050a0c9
LukePulverenti <luke.pulverenti@gmail.com>
2013-02-22 01:35:25 -0500

    adding core plugins

---
+++ MediaBrowser.Plugins.Dlna/Configuration/DlnaConfigurationPage.cs
@@ -0,0 +1,77 @@
+using MediaBrowser.Controller.Plugins;
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+
+namespace MediaBrowser.Plugins.Dlna.Configuration
+{
+    /// <summary>
+    /// Class DlnaConfigurationPage
+    /// </summary>
+    [Export(typeof(IPluginConfigurationPage))]
+    class DlnaConfigurationPage : IPluginConfigurationPage
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            get { return "Dlna"; }
+        }
+
+        /// <summary>
+        /// Gets the HTML stream.
+        /// </summary>
+        /// <returns>Stream.</returns>
+        public Stream GetHtmlStream()
+        {
+            return GetType().Assembly.GetManifestResourceStream("MediaBrowser.Plugins.Dlna.Configuration.configPage.html");
+        }
+        
+        /// <summary>
+        /// Gets the date last modified.
+        /// </summary>
+        /// <value>The date last modified.</value>
+        public DateTime DateLastModified
+        {
+            get { return Plugin.Instance.AssemblyDateLastModified; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description
+        {
+            get { return string.Empty; }
+        }
+
+        /// <summary>
+        /// Gets the plugin id.
+        /// </summary>
+        /// <value>The plugin id.</value>
+        public Guid? PluginId
+        {
+            get { return Plugin.Instance.Id; }
+        }
+
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        /// <value>The version.</value>
+        public string Version
+        {
+            get { return Plugin.Instance.Version.ToString(); }
+        }
+
+        /// <summary>
+        /// Gets the type of the configuration page.
+        /// </summary>
+        /// <value>The type of the configuration page.</value>
+        public ConfigurationPageType ConfigurationPageType
+        {
+            get { return ConfigurationPageType.PluginConfiguration; }
+        }
+    }
+}
---
+++ MediaBrowser.Plugins.Trailers/Configuration/TrailerConfigurationPage.cs
@@ -0,0 +1,77 @@
+﻿using MediaBrowser.Controller.Plugins;
+using System;
+using System.ComponentModel.Composition;
+using System.IO;
+
+namespace MediaBrowser.Plugins.Trailers.Configuration
+{
+    /// <summary>
+    /// Class TrailerConfigurationPage
+    /// </summary>
+    [Export(typeof(IPluginConfigurationPage))]
+    class TrailerConfigurationPage : IPluginConfigurationPage
+    {
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            get { return "Trailers"; }
+        }
+
+        /// <summary>
+        /// Gets the HTML stream.
+        /// </summary>
+        /// <returns>Stream.</returns>
+        public Stream GetHtmlStream()
+        {
+            return GetType().Assembly.GetManifestResourceStream("MediaBrowser.Plugins.Trailers.Configuration.configPage.html");
+        }
+        
+        /// <summary>
+        /// Gets the date last modified.
+        /// </summary>
+        /// <value>The date last modified.</value>
+        public DateTime DateLastModified
+        {
+            get { return Plugin.Instance.AssemblyDateLastModified; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public string Description
+        {
+            get { return string.Empty; }
+        }
+
+        /// <summary>
+        /// Gets the plugin id.
+        /// </summary>
+        /// <value>The plugin id.</value>
+        public Guid? PluginId
+        {
+            get { return Plugin.Instance.Id; }
+        }
+
+        /// <summary>
+        /// Gets the version.
+        /// </summary>
+        /// <value>The version.</value>
+        public string Version
+        {
+            get { return Plugin.Instance.Version.ToString(); }
+        }
+
+        /// <summary>
+        /// Gets the type of the configuration page.
+        /// </summary>
+        /// <value>The type of the configuration page.</value>
+        public ConfigurationPageType ConfigurationPageType
+        {
+            get { return ConfigurationPageType.PluginConfiguration; }
+        }
+    }
+}
