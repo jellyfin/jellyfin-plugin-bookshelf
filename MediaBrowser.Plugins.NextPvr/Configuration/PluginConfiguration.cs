52b06438067084f88d96cd681d56092bcf33447a
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-09-26 12:17:16 -0400

    added next pvr stub

---
+++ MediaBrowser.Plugins.NextPvr/Configuration/PluginConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace MediaBrowser.Plugins.NextPvr.Configuration
+{
+    /// <summary>
+    /// Class PluginConfiguration
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+    }
+}
---
+++ MediaBrowser.Plugins.NextPvr/LiveTvService.cs
@@ -0,0 +1,41 @@
+﻿using MediaBrowser.Controller.LiveTv;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MediaBrowser.Plugins.NextPvr
+{
+    /// <summary>
+    /// Class LiveTvService
+    /// </summary>
+    public class LiveTvService : ILiveTvService
+    {
+        /// <summary>
+        /// Gets the channels async.
+        /// </summary>
+        /// <param name="cancellationToken">The cancellation token.</param>
+        /// <returns>Task{IEnumerable{ChannelInfo}}.</returns>
+        public Task<IEnumerable<ChannelInfo>> GetChannelsAsync(CancellationToken cancellationToken)
+        {
+            var channels = new List<ChannelInfo>
+                {
+                    new ChannelInfo
+                        {
+                             Name = "NBC",
+                              ServiceName = Name
+                        }
+                };
+
+            return Task.FromResult<IEnumerable<ChannelInfo>>(channels);
+        }
+
+        /// <summary>
+        /// Gets the name.
+        /// </summary>
+        /// <value>The name.</value>
+        public string Name
+        {
+            get { return "Next Pvr"; }
+        }
+    }
+}
