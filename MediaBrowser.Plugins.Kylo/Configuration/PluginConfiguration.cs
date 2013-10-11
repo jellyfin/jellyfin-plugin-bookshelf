5bd92af84c607cc3dd860664bfb114672820534d
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-10-11 11:29:07 -0400

    removed mbt plugins

--- MediaBrowser.Plugins.Kylo/Configuration/PluginConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediaBrowser.Model.Plugins;
-
-namespace MediaBrowser.Plugins.Kylo.Configuration
-{
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-    }
-}
--- MediaBrowser.Plugins.WindowsMediaCenter/Configuration/PluginConfiguration.cs
+++
@@ -1,8 +0,0 @@
-﻿using MediaBrowser.Model.Plugins;
-
-namespace MediaBrowser.Plugins.WindowsMediaCenter.Configuration
-{
-    public class PluginConfiguration : BasePluginConfiguration
-    {
-    }
-}
