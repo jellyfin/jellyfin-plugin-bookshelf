54f1380a0f9059374daf372f0e12c42b32dfbbf8
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-10-28 08:37:01 -0400

    Added NesBox plugin stub

---
+++ MediaBrowser.Plugins.NesBox/Configuration/PluginConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using MediaBrowser.Model.Plugins;
+
+namespace MediaBrowser.Plugins.NesBox.Configuration
+{
+    /// <summary>
+    /// Class PluginConfiguration
+    /// </summary>
+    public class PluginConfiguration : BasePluginConfiguration
+    {
+    }
+}
---
+++ MediaBrowser.Plugins.NesBox/Plugin.cs
@@ -0,0 +1,46 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Plugins.NesBox.Configuration;
+
+namespace MediaBrowser.Plugins.NesBox
+{
+    /// <summary>
+    /// Class Plugin
+    /// </summary>
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base(applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "NESBox"; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get
+            {
+                return "Adds NESBox links for Nintendo and games.";
+            }
+        }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static Plugin Instance { get; private set; }
+    }
+}
