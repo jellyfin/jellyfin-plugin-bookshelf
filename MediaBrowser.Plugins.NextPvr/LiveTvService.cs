780c8997ea0464008f7e20fa035533575749929c
Luke Pulverenti <luke.pulverenti@gmail.com>
2013-11-12 17:33:20 -0500

    made next pvr connection on demand

--- MediaBrowser.Plugins.NextPvr/LiveTvService.cs
+++ MediaBrowser.Plugins.NextPvr/LiveTvService.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
-using MediaBrowser.Common.Net;
+﻿using MediaBrowser.Common.Net;
 using MediaBrowser.Controller.LiveTv;
 using MediaBrowser.Model.LiveTv;
 using MediaBrowser.Plugins.NextPvr.Helpers;
 using System;
-using System.Globalization;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -22,85 +21,103 @@ namespace MediaBrowser.Plugins.NextPvr
         private readonly IHttpClient _httpClient;
 
         private string Sid { get; set; }
-        private string Salt { get; set; }
 
         private string WebserviceUrl { get; set; }
         private int Port { get; set; }
         private string Pin { get; set; }
 
-        private bool IsConnected { get; set; }
-
         public LiveTvService(IHttpClient httpClient)
         {
             _httpClient = httpClient;
+        }
 
-            WebserviceUrl = Plugin.Instance.Configuration.WebServiceUrl;
-            Port = Plugin.Instance.Configuration.Port;
-            Pin = Plugin.Instance.Configuration.Pin;
-            
-            if(string.IsNullOrEmpty(WebserviceUrl) && string.IsNullOrEmpty(Port.ToString(CultureInfo.InvariantCulture)) && string.IsNullOrEmpty(Pin))
+        private async Task EnsureConnectionAsync(CancellationToken cancellationToken)
+        {
+            var config = Plugin.Instance.Configuration;
+
+            if (string.IsNullOrEmpty(config.WebServiceUrl))
             {
-                InitiateSession();
-                Login();
+                throw new InvalidOperationException("NextPvr web service url must be configured.");
+            }
+
+            if (string.IsNullOrEmpty(Sid))
+            {
+                await InitiateSession(cancellationToken).ConfigureAwait(false);
             }
         }
 
         /// <summary>
         /// Initiate the nextPvr session
         /// </summary>
-        internal void InitiateSession()
+        private async Task InitiateSession(CancellationToken cancellationToken)
         {
             string html;
 
-            HttpRequestOptions options = new HttpRequestOptions()
-                {
-                    // This moment only device name xbmc is available
-                    Url = string.Format("{0}:{1}/service?method=session.initiate&ver=1.0&device=xbmc", WebserviceUrl,Port)
-                };
+            WebserviceUrl = Plugin.Instance.Configuration.WebServiceUrl;
+            Port = Plugin.Instance.Configuration.Port;
+            Pin = Plugin.Instance.Configuration.Pin;
 
-            using (var reader = new StreamReader( _httpClient.Get(options).Result))
+            var options = new HttpRequestOptions
             {
-                html = reader.ReadToEnd();
-            }
+                // This moment only device name xbmc is available
+                Url = string.Format("{0}:{1}/service?method=session.initiate&ver=1.0&device=xbmc", WebserviceUrl, Port),
 
+                CancellationToken = cancellationToken
+            };
+
+            using (var stream = await _httpClient.Get(options).ConfigureAwait(false))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    html = await reader.ReadToEndAsync().ConfigureAwait(false);
+                }
+            }
 
             if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
             {
-                Salt = XmlHelper.GetSingleNode(html, "//rsp/salt").InnerXml;
-                Sid = XmlHelper.GetSingleNode(html, "//rsp/sid").InnerXml;
+                var salt = XmlHelper.GetSingleNode(html, "//rsp/salt").InnerXml;
+                var sid = XmlHelper.GetSingleNode(html, "//rsp/sid").InnerXml;
+
+                var loggedIn = await Login(sid, salt, cancellationToken).ConfigureAwait(false);
+
+                if (loggedIn)
+                {
+                    Sid = sid;
+                }
             }
         }
 
-        internal void Login()
+        private async Task<bool> Login(string sid, string salt, CancellationToken cancellationToken)
         {
             string html;
 
             var md5 = EncryptionHelper.GetMd5Hash(Pin);
-            StringBuilder strb = new StringBuilder();
+            var strb = new StringBuilder();
 
             strb.Append(":");
             strb.Append(md5);
             strb.Append(":");
-            strb.Append(Salt);
+            strb.Append(salt);
 
             var md5Result = EncryptionHelper.GetMd5Hash(strb.ToString());
 
-            HttpRequestOptions options = new HttpRequestOptions()
+            var options = new HttpRequestOptions()
             {
                 // This moment only device name xbmc is available
-                Url = string.Format("{0}:{1}/service?method=session.login&&sid={2}&md5={3}", WebserviceUrl, Port, Sid,md5Result)            
+                Url = string.Format("{0}:{1}/service?method=session.login&&sid={2}&md5={3}", WebserviceUrl, Port, sid, md5Result),
+
+                CancellationToken = cancellationToken
             };
 
-            using (var reader = new StreamReader(_httpClient.Get(options).Result))
+            using (var stream = await _httpClient.Get(options).ConfigureAwait(false))
             {
-                html = reader.ReadToEnd();
+                using (var reader = new StreamReader(stream))
+                {
+                    html = await reader.ReadToEndAsync().ConfigureAwait(false);
+                }
             }
 
-            if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
-          {
-              IsConnected = true;
-          }
-
+            return XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok";
         }
 
         /// <summary>
@@ -110,130 +127,139 @@ namespace MediaBrowser.Plugins.NextPvr
         /// <returns>Task{IEnumerable{ChannelInfo}}.</returns>
         public async Task<IEnumerable<ChannelInfo>> GetChannelsAsync(CancellationToken cancellationToken)
         {
-            List<ChannelInfo> channels = new List<ChannelInfo>();
+            await EnsureConnectionAsync(cancellationToken).ConfigureAwait(false);
 
-            if (IsConnected)
+            var channels = new List<ChannelInfo>();
+
+            string html;
+
+            var options = new HttpRequestOptions()
             {
-                string html;
-               
+                CancellationToken = cancellationToken,
+                Url = string.Format("{0}:{1}/service?method=channel.list&sid={2}", WebserviceUrl, Port, Sid)
+            };
 
-                HttpRequestOptions options = new HttpRequestOptions()
-                    {
-                        CancellationToken = cancellationToken,
-                        Url = string.Format("{0}:{1}/service?method=channel.list&sid={2}", WebserviceUrl, Port, Sid)
-                    };
-
-                using (var reader = new StreamReader(_httpClient.Get(options).Result))
+            using (var stream = await _httpClient.Get(options).ConfigureAwait(false))
+            {
+                using (var reader = new StreamReader(stream))
                 {
-                    html = reader.ReadToEnd();
-                }
-
-                if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
-                {
-                    channels.AddRange(from XmlNode node in XmlHelper.GetMultipleNodes(html, "//rsp/channels/channel")
-                                      select new ChannelInfo()
-                                          {
-                                              Id = XmlHelper.GetSingleNode(node.OuterXml, "//id").InnerXml,
-                                              Name = XmlHelper.GetSingleNode(node.OuterXml, "//name").InnerXml,
-                                              ChannelType = ChannelHelper.GetChannelType(XmlHelper.GetSingleNode(node.OuterXml, "//type").InnerXml),
-                                              ServiceName = Name
-                                          });
+                    html = await reader.ReadToEndAsync().ConfigureAwait(false);
                 }
             }
 
-            return await Task.FromResult<IEnumerable<ChannelInfo>>(channels);
+            if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
+            {
+                channels.AddRange(from XmlNode node in XmlHelper.GetMultipleNodes(html, "//rsp/channels/channel")
+                                  select new ChannelInfo()
+                                  {
+                                      Id = XmlHelper.GetSingleNode(node.OuterXml, "//id").InnerXml,
+                                      Name = XmlHelper.GetSingleNode(node.OuterXml, "//name").InnerXml,
+                                      ChannelType = ChannelHelper.GetChannelType(XmlHelper.GetSingleNode(node.OuterXml, "//type").InnerXml),
+                                      ServiceName = Name
+                                  });
+            }
+
+            return channels;
         }
 
         public async Task<IEnumerable<RecordingInfo>> GetRecordingsAsync(CancellationToken cancellationToken)
         {
-            List<RecordingInfo> recordings = new List<RecordingInfo>();
+            await EnsureConnectionAsync(cancellationToken).ConfigureAwait(false);
 
-            if (IsConnected)
+            var recordings = new List<RecordingInfo>();
+
+            string html;
+
+            var options = new HttpRequestOptions()
             {
-                string html;
+                CancellationToken = cancellationToken,
+                Url = string.Format("{0}:{1}/service?method=recording.list&sid={2}", WebserviceUrl, Port, Sid)
+            };
 
-                HttpRequestOptions options = new HttpRequestOptions()
+            using (var stream = await _httpClient.Get(options).ConfigureAwait(false))
+            {
+                using (var reader = new StreamReader(stream))
                 {
-                    CancellationToken = cancellationToken,
-                    Url = string.Format("{0}:{1}/service?method=recording.list&sid={2}", WebserviceUrl, Port, Sid)
-                };
-
-                using (var reader = new StreamReader(_httpClient.Get(options).Result))
-                {
-                    html = reader.ReadToEnd();
-                }
-
-                if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
-                {
-                    recordings.AddRange(
-                        from XmlNode node in XmlHelper.GetMultipleNodes(html, "//rsp/recordings/recording")
-                        let startDate = DateTime.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//start_time").InnerXml)
-                        select new RecordingInfo()
-                            {
-                                Id = XmlHelper.GetSingleNode(node.OuterXml, "//id").InnerXml,
-                                Name = XmlHelper.GetSingleNode(node.OuterXml, "//name").InnerXml,
-                                Description = XmlHelper.GetSingleNode(node.OuterXml, "//desc").InnerXml,
-                                StartDate = startDate,
-                                Status = XmlHelper.GetSingleNode(node.OuterXml, "//status").InnerXml,
-                                Quality = XmlHelper.GetSingleNode(node.OuterXml, "//quality").InnerXml,
-                                ChannelName = XmlHelper.GetSingleNode(node.OuterXml, "//channel").InnerXml,
-                                ChannelId = XmlHelper.GetSingleNode(node.OuterXml, "//channel_id").InnerXml,
-                                Recurring = bool.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//recurring").InnerXml),
-                                RecurrringStartDate =
-                                    DateTime.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//recurring_start").InnerXml),
-                                RecurringEndDate =
-                                    DateTime.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//recurring_end").InnerXml),
-                                RecurringParent = XmlHelper.GetSingleNode(node.OuterXml, "//recurring_parent").InnerXml,
-                                DayMask = XmlHelper.GetSingleNode(node.OuterXml, "//daymask").InnerXml.Split(',').ToList(),
-                                EndDate =
-                                    startDate.AddSeconds(
-                                        (double.Parse(
-                                            XmlHelper.GetSingleNode(node.OuterXml, "//duration_seconds").InnerXml)))
-                            });
+                    html = await reader.ReadToEndAsync().ConfigureAwait(false);
                 }
             }
 
-            return await Task.FromResult<IEnumerable<RecordingInfo>>(recordings);
-        }
-
-        public Task<EpgFullInfo> GetEpgAsync(string channelId, CancellationToken cancellationToken)
-        {
-            List<EpgInfo> epgInfos = new List<EpgInfo>();
-
-            if (IsConnected)
+            if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
             {
-                string html;
-
-                HttpRequestOptions options = new HttpRequestOptions()
-                {
-                    CancellationToken = cancellationToken,
-                    Url = string.Format("{0}:{1}/service?method=channel.listings&channel_id={2}&sid={3}", WebserviceUrl, Port, channelId, Sid)
-                };
-
-                using (var reader = new StreamReader(_httpClient.Get(options).Result))
-                {
-                    html = reader.ReadToEnd();
-                }
-
-                if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
-                {
-                    epgInfos.AddRange(
-                        from XmlNode node in XmlHelper.GetMultipleNodes(html, "//rsp/listings/l")
-                        let startDate = XmlHelper.GetSingleNode(node.OuterXml, "//start").InnerXml
-                        let endDate = XmlHelper.GetSingleNode(node.OuterXml, "//end").InnerXml
-                        select new EpgInfo()
+                recordings.AddRange(
+                    from XmlNode node in XmlHelper.GetMultipleNodes(html, "//rsp/recordings/recording")
+                    let startDate = DateTime.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//start_time").InnerXml)
+                    select new RecordingInfo()
                         {
                             Id = XmlHelper.GetSingleNode(node.OuterXml, "//id").InnerXml,
                             Name = XmlHelper.GetSingleNode(node.OuterXml, "//name").InnerXml,
-                            Description = XmlHelper.GetSingleNode(node.OuterXml, "//description").InnerXml,
-                            StartDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(Math.Round(double.Parse(startDate)) / 1000d).ToLocalTime(),
-                            EndDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(Math.Round(double.Parse(endDate)) / 1000d).ToLocalTime(),
-                            Genre = XmlHelper.GetSingleNode(node.OuterXml, "//genre").InnerXml,
+                            Description = XmlHelper.GetSingleNode(node.OuterXml, "//desc").InnerXml,
+                            StartDate = startDate,
+                            Status = XmlHelper.GetSingleNode(node.OuterXml, "//status").InnerXml,
+                            Quality = XmlHelper.GetSingleNode(node.OuterXml, "//quality").InnerXml,
+                            ChannelName = XmlHelper.GetSingleNode(node.OuterXml, "//channel").InnerXml,
+                            ChannelId = XmlHelper.GetSingleNode(node.OuterXml, "//channel_id").InnerXml,
+                            Recurring = bool.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//recurring").InnerXml),
+                            RecurrringStartDate =
+                                DateTime.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//recurring_start").InnerXml),
+                            RecurringEndDate =
+                                DateTime.Parse(XmlHelper.GetSingleNode(node.OuterXml, "//recurring_end").InnerXml),
+                            RecurringParent = XmlHelper.GetSingleNode(node.OuterXml, "//recurring_parent").InnerXml,
+                            DayMask = XmlHelper.GetSingleNode(node.OuterXml, "//daymask").InnerXml.Split(',').ToList(),
+                            EndDate =
+                                startDate.AddSeconds(
+                                    (double.Parse(
+                                        XmlHelper.GetSingleNode(node.OuterXml, "//duration_seconds").InnerXml)))
                         });
+            }
+
+            return recordings;
+        }
+
+        public async Task<EpgFullInfo> GetEpgAsync(string channelId, CancellationToken cancellationToken)
+        {
+            await EnsureConnectionAsync(cancellationToken).ConfigureAwait(false);
+
+            var epgInfos = new List<EpgInfo>();
+
+            string html;
+
+            var options = new HttpRequestOptions()
+            {
+                CancellationToken = cancellationToken,
+                Url = string.Format("{0}:{1}/service?method=channel.listings&channel_id={2}&sid={3}", WebserviceUrl, Port, channelId, Sid)
+            };
+
+            using (var stream = await _httpClient.Get(options).ConfigureAwait(false))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    html = await reader.ReadToEndAsync().ConfigureAwait(false);
                 }
             }
 
-            return Task.FromResult<EpgFullInfo>(new EpgFullInfo() { ChannelId = channelId, EpgInfos = epgInfos });
+            if (XmlHelper.GetSingleNode(html, "//rsp/@stat").InnerXml.ToLower() == "ok")
+            {
+                epgInfos.AddRange(
+                    from XmlNode node in XmlHelper.GetMultipleNodes(html, "//rsp/listings/l")
+                    let startDate = XmlHelper.GetSingleNode(node.OuterXml, "//start").InnerXml
+                    let endDate = XmlHelper.GetSingleNode(node.OuterXml, "//end").InnerXml
+                    select new EpgInfo()
+                    {
+                        Id = XmlHelper.GetSingleNode(node.OuterXml, "//id").InnerXml,
+                        Name = XmlHelper.GetSingleNode(node.OuterXml, "//name").InnerXml,
+                        Description = XmlHelper.GetSingleNode(node.OuterXml, "//description").InnerXml,
+                        StartDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(Math.Round(double.Parse(startDate)) / 1000d).ToLocalTime(),
+                        EndDate = new DateTime(1970, 1, 1, 0, 0, 0, 0).AddSeconds(Math.Round(double.Parse(endDate)) / 1000d).ToLocalTime(),
+                        Genre = XmlHelper.GetSingleNode(node.OuterXml, "//genre").InnerXml,
+                    });
+            }
+
+            return new EpgFullInfo
+            {
+                ChannelId = channelId,
+                EpgInfos = epgInfos
+            };
         }
 
         /// <summary>
