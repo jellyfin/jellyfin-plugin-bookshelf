049cbffa973aaca18aefeeb061af0fdef483e730
Sven Van den brande <sven.vandenbrande@outlook.com>
2014-02-02 23:11:53 +0100

    InstantiateResponse information Parser

---
+++ MediaBrowser.Plugins.NextPvr/InstantiateResponse.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using MediaBrowser.Model.Serialization;
+
+namespace MediaBrowser.Plugins.NextPvr
+{
+    public class InstantiateResponse
+    {
+        public static ClientKeys GetClientKeys(Stream stream, IJsonSerializer json)
+        {
+            var root = json.DeserializeFromStream<RootObject>(stream);
+
+            if (root.clientKeys != null && root.clientKeys.sid != null && root.clientKeys.salt != null)
+            {
+                return root.clientKeys;
+            }
+            throw new ApplicationException("Failed to load the ClientKeys from NextPvr.");
+        }
+
+        public class ClientKeys
+        {
+            public string sid { get; set; }
+            public string salt { get; set; }
+        }
+
+        private class RootObject
+        {
+            public ClientKeys clientKeys { get; set; }
+        }
+    }
+}
