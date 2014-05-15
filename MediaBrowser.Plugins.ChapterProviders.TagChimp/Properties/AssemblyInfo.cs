217b570316a79d674235e559a3ec836f0ad388e7
Tim Hobbs <jesus.tesh@gmail.com>
2014-05-14 18:36:00 -0700

    Changed to shared version

--- MediaBrowser.Plugins.ChapterProviders.TagChimp/Properties/AssemblyInfo.cs
+++ MediaBrowser.Plugins.ChapterProviders.TagChimp/Properties/AssemblyInfo.cs
@@ -21,15 +21,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("a909b97c-540f-45c9-8205-06fd9793b481")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
