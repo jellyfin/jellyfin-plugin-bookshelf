497dee62d99003eaff0f92a11fa1f1b1d6ac7450
Stephen <snazy2000@btinternet.com>
2014-05-21 17:41:35 +0100

    Added Revision 3. Needs some work with page number stuff and needs image

---
+++ MediaBrowser.Plugins.Revision3/Revision3ListingDownloader.cs
@@ -0,0 +1,36 @@
+﻿using MediaBrowser.Common.Net;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Serialization;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+namespace MediaBrowser.Plugins.Revision3
+{
+    public class Revision3ListingDownloader
+    {
+
+        private ILogger _logger;
+        private readonly IHttpClient _httpClient;
+        private readonly IJsonSerializer _jsonSerializer;
+
+        public Revision3ListingDownloader(ILogger logManager, IJsonSerializer jsonSerializer, IHttpClient httpClient)
+        {
+            _logger = logManager;
+            _jsonSerializer = jsonSerializer;
+            _httpClient = httpClient;
+        }
+
+        public async Task<RootObject> GetEpisodeList(String catID, CancellationToken cancellationToken)
+        {
+            RootObject reg;
+
+            using (var json = await _httpClient.Get("http://revision3.com/api/getEpisodes.json?api_key=0b1faede6785d04b78735b139ddf2910f34ad601&show_id=" + catID, CancellationToken.None).ConfigureAwait(false))
+            {
+                reg = _jsonSerializer.DeserializeFromStream<RootObject>(json);
+            }
+
+            return reg;
+        }
+       
+    }
+}
