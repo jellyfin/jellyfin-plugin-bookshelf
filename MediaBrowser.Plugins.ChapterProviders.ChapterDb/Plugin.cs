c2a2ed06c4a62328ccbe0be2a7f0649c11137d2c
Tim Hobbs <jesus.tesh@gmail.com>
2014-05-27 15:39:58 -0700

    Added ChapterDB provider

---
+++ MediaBrowser.Plugins.ChapterProviders.ChapterDb/Plugin.cs
@@ -0,0 +1,42 @@
+﻿using MediaBrowser.Common.Configuration;
+using MediaBrowser.Common.Plugins;
+using MediaBrowser.Model.Logging;
+using MediaBrowser.Model.Serialization;
+using MediaBrowser.Plugins.ChapterProviders.ChapterDb.Configuration;
+
+namespace MediaBrowser.Plugins.ChapterProviders.ChapterDb {
+    public class Plugin : BasePlugin<PluginConfiguration>
+    {
+        public Plugin(IApplicationPaths applicationPaths, IXmlSerializer xmlSerializer)
+            : base (applicationPaths, xmlSerializer)
+        {
+            Instance = this;
+        }
+
+        /// <summary>
+        /// Gets the name of the plugin
+        /// </summary>
+        /// <value>The name.</value>
+        public override string Name
+        {
+            get { return "ChapterDb Chapters Provider"; }
+        }
+
+        /// <summary>
+        /// Gets the description.
+        /// </summary>
+        /// <value>The description.</value>
+        public override string Description
+        {
+            get { return "Gets chapter information from chapterdb.org"; }
+        }
+
+        protected ILogger Logger { get; set; }
+
+        /// <summary>
+        /// Gets the instance.
+        /// </summary>
+        /// <value>The instance.</value>
+        public static Plugin Instance { get; private set; }
+    }
+}
