55370d4d063412827e7e10452211a436be0f50c4
Tim Hobbs <jesus.tesh@gmail.com>
2014-05-27 15:39:14 -0700

    TagChimp provider cleanup

--- MediaBrowser.Plugins.ChapterProviders.TagChimp/Plugin.cs
+++ MediaBrowser.Plugins.ChapterProviders.TagChimp/Plugin.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MediaBrowser.Common.Configuration;
+﻿using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
 using MediaBrowser.Model.Logging;
 using MediaBrowser.Model.Serialization;
