8424193dda1f4e2a7bd4490b1791a05b0d6c3854
Stephen <snazy2000@btinternet.com>
2015-01-13 20:05:53 +0000

    Add devices image

--- MediaBrowser.Plugins.PushBulletNotifications/Api/ServerApiEntryPoints.cs
+++ MediaBrowser.Plugins.PushBulletNotifications/Api/ServerApiEntryPoints.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using MediaBrowser.Common.Net;
 using MediaBrowser.Controller.Net;
 using MediaBrowser.Model.Logging;
@@ -43,14 +46,39 @@ namespace MediaBrowser.Plugins.PushBulletNotifications.Api
                 {"body", "This is a test notification from MediaBrowser"}
             };
 
-            var _httpRequest = new HttpRequestOptions();
-            //Create Basic HTTP Auth Header...
-            string _cred = string.Format("{0} {1}", "Basic", options.Token);
+            //var _httpRequest = (HttpWebRequest)WebRequest.Create("https://api.pushbullet.com/api/pushes");
+            //_httpRequest.Credentials = new NetworkCredential();
+            //_httpRequest.Method = "POST";
 
-            _httpRequest.RequestHeaders["Authorization"] = _cred;
+            //var requestWriter = new StreamWriter(_httpRequest.GetRequestStream());
+
+
+            //try
+            //{
+            //    requestWriter.Write(SM_TEMPLATE);
+            //}
+            //finally
+            //{
+            //    requestWriter.Close();
+            //    requestWriter = null;
+            //}
+
+
+            var _httpRequest = new HttpRequestOptions();
+            
+            //Create Basic HTTP Auth Header...
+           // string _cred = string.Format("{0} {1}", "Basic", options.Token);
+
+            string authInfo = options.Token;
+            authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            
+           // authInfo = Convert.ToBase64String(Encoding.Default.GetBytes(authInfo));
+            _httpRequest.RequestHeaders["Authorization"] = "Basic " + authInfo;
+
+            //_httpRequest.
             _httpRequest.Url = "https://api.pushbullet.com/api/pushes";
 
-            _logger.Debug("PushBullet <TEST> to {0} - {1}", options.Token, _cred);
+            _logger.Debug("PushBullet <TEST> to {0} - {1}", options.Token, authInfo);
 
             return _httpClient.Post(_httpRequest, parameters);
         }
