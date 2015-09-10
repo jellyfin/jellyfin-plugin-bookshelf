491e08218d78a01a3d8c732921a2710c2d7aa4f3
Rüdiger Gubler <ruediger@gubler-online.de>
2015-09-10 23:39:40 +0200

    Update to library 3.0.633 (Common and Server.Core); Start of work for TVHeadend 4.*

---
+++ TVHeadEnd/TunerHost.cs
@@ -0,0 +1,54 @@
+﻿using MediaBrowser.Controller.LiveTv;
+using MediaBrowser.Model.Dto;
+using MediaBrowser.Model.LiveTv;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TVHeadEnd
+{
+    class TunerHost : ITunerHost
+    {
+        public string Name
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Type
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public Task<IEnumerable<ChannelInfo>> GetChannels(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<MediaSourceInfo> GetChannelStream(string channelId, string streamId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<MediaSourceInfo>> GetChannelStreamMediaSources(string channelId, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<LiveTvTunerInfo>> GetTunerInfos(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Validate(TunerHostInfo info)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
