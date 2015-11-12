80ca2f63f8f8da40e201669126dca41e79f5b125
Sven Van den brande <sven.vandenbrande@outlook.com>
2015-11-12 23:26:13 +0100

    Support Subchannels + Update Tuner status information

--- MediaBrowser.Plugins.NextPvr/Responses/TunerResponse.cs
+++ MediaBrowser.Plugins.NextPvr/Responses/TunerResponse.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using MediaBrowser.Controller.LiveTv;
+using MediaBrowser.Model.Drawing;
 using MediaBrowser.Model.LiveTv;
 using MediaBrowser.Model.Serialization;
 
@@ -13,18 +14,45 @@ namespace MediaBrowser.Plugins.NextPvr.Responses
 
         public TunerResponse(Stream stream, IJsonSerializer json)
         {
-           _root = json.DeserializeFromStream<RootObject>(stream);
+            _root = json.DeserializeFromStream<RootObject>(stream);
 
         }
         public List<LiveTvTunerInfo> LiveTvTunerInfos()
         {
-            return _root.Tuners.Select(i => new LiveTvTunerInfo()
-            {
-                Name = i.tunerName
-            }).ToList();
+            return _root.Tuners.Select(GetTunerInformation).ToList();
         }
 
-        public class Recording2
+        private LiveTvTunerInfo GetTunerInformation(Tuner i)
+        {
+            LiveTvTunerInfo tunerinfo = new LiveTvTunerInfo();
+
+            tunerinfo.Name = i.tunerName;
+            tunerinfo.Status = GetStatus(i);
+
+            if (i.recordings.Count > 0)
+            {
+                tunerinfo.ChannelId = i.recordings.Single().Recording.channelOID.ToString();
+            }
+
+            return tunerinfo;
+        }
+
+        private LiveTvTunerStatus GetStatus(Tuner i)
+        {
+            if (i.recordings.Count > 0)
+            {
+                return LiveTvTunerStatus.RecordingTv;
+            }
+
+            if (i.liveTV.Count > 0)
+            {
+                return LiveTvTunerStatus.LiveTv;
+            }
+
+            return LiveTvTunerStatus.Available;
+        }
+
+        public class Recording
         {
             public int tunerOID { get; set; }
             public string recName { get; set; }
@@ -32,16 +60,16 @@ namespace MediaBrowser.Plugins.NextPvr.Responses
             public int recordingOID { get; set; }
         }
 
-        public class Recording
+        public class Recordings
         {
-            public Recording2 Recording2 { get; set; }
+            public Recording Recording { get; set; }
         }
 
         public class Tuner
         {
             public string tunerName { get; set; }
             public string tunerStatus { get; set; }
-            public List<Recording> recordings { get; set; }
+            public List<Recordings> recordings { get; set; }
             public List<object> liveTV { get; set; }
         }
 
