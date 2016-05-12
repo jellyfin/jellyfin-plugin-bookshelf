c81325b95fdaa9c648363cecb0915c45ef0656fd
Fernando Casas Schössow <fcasas@homenet.local>
2016-05-12 02:19:09 +0200

    Added configuration option to enable or disable forced video deinterlacing for all channels and recordings

--- TVHeadEnd/HTSConnectionHandler.cs
+++ TVHeadEnd/HTSConnectionHandler.cs
@@ -33,6 +33,7 @@ namespace TVHeadEnd
         private string _userName;
         private string _password;
         private bool _enableSubsMaudios;
+        private bool _forceDeinterlace;
 
         // Data helpers
         private readonly ChannelDataHelper _channelDataHelper;
@@ -122,6 +123,7 @@ namespace TVHeadEnd
             _profile = config.Profile.Trim();
             _channelType = config.ChannelType.Trim();
             _enableSubsMaudios = config.EnableSubsMaudios;
+            _forceDeinterlace = config.ForceDeinterlace;
 
             if (_priority < 0 || _priority > 4)
             {
@@ -238,6 +240,11 @@ namespace TVHeadEnd
         public bool GetEnableSubsMaudios()
         {
             return _enableSubsMaudios;
+        }
+
+        public bool GetForceDeinterlace()
+        {
+            return _forceDeinterlace;
         }
 
         public Task<IEnumerable<RecordingInfo>> BuildDvrInfos(CancellationToken cancellationToken)
