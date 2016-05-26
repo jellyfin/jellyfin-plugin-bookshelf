07fbd2027ff5bad0e06ee7e6ae5abd841ae9812d
softworkz <softworkz@hotmail.com>
2016-05-26 03:27:32 +0200

    New MovieOrganizer PlugIn

---
+++ MovieOrganizer/Api/MovieOrganizerApi.cs
@@ -0,0 +1,54 @@
+﻿using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Net;
+using MediaBrowser.Model.Logging;
+using MovieOrganizer.Service;
+using ServiceStack;
+using System.Threading;
+
+namespace MovieOrganizer.Api
+{
+    [Route("/Library/FileOrganizations/{Id}/Movie/OrganizeExt", "POST", Summary = "Performs organization of a movie file")]
+    public class OrganizeMovie
+    {
+        [ApiMember(Name = "Id", Description = "Result Id", IsRequired = true, DataType = "string", ParameterType = "path", Verb = "POST")]
+        public string Id { get; set; }
+
+        [ApiMember(Name = "MovieName", Description = "Movie Name", IsRequired = true, DataType = "string", ParameterType = "query", Verb = "POST")]
+        public string MovieName { get; set; }
+
+        [ApiMember(Name = "MovieYear", Description = "Movie Year", IsRequired = false, DataType = "string", ParameterType = "query", Verb = "POST")]
+        public string MovieYear { get; set; }
+
+        [ApiMember(Name = "TargetFolder", Description = "Target Folder", IsRequired = true, DataType = "string", ParameterType = "query", Verb = "POST")]
+        public string TargetFolder { get; set; }
+    }
+
+    [Authenticated]
+    public class MovieOrganizerApi : IRestfulService
+    {
+        private readonly ILogger _logger;
+        private readonly MovieOrganizerService _service;
+        private readonly ILibraryManager _libraryManager;
+
+        public MovieOrganizerApi(ILogManager logManager, MovieOrganizerService service, ILibraryManager libraryManager)
+        {
+            _logger = logManager.GetLogger(GetType().Name);
+            _service = service;
+            _libraryManager = libraryManager;
+        }
+
+        public void Post(OrganizeMovie request)
+        {
+            var task = _service.PerformMovieOrganization(new MovieFileOrganizationRequest
+            {
+                ResultId = request.Id,
+                Name = request.MovieName,
+                Year = request.MovieYear,
+                TargetFolder = request.TargetFolder
+            });
+
+            // Wait 2s for exceptions that may occur and would be automatically forwarded to the client for immediate error display
+            task.Wait(2000);
+        }
+    }
+}
---
+++ MovieOrganizer/ServerEntryPoint.cs
@@ -0,0 +1,148 @@
+﻿using CommonIO;
+using MediaBrowser.Controller;
+using MediaBrowser.Controller.Configuration;
+using MediaBrowser.Controller.FileOrganization;
+using MediaBrowser.Controller.Library;
+using MediaBrowser.Controller.Localization;
+using MediaBrowser.Controller.Net;
+using MediaBrowser.Controller.Plugins;
+using MediaBrowser.Controller.Providers;
+using MediaBrowser.Model.Logging;
+using MovieOrganizer;
+using MovieOrganizer.Api;
+using MovieOrganizer.Service;
+using ServiceStack;
+using ServiceStack.Host.Handlers;
+using ServiceStack.Web;
+using System.IO;
+using System.Web;
+
+namespace MovieOrganizer
+{
+    /// <summary>
+    /// All communication between the server and the plugins server instance should occur in this class.
+    /// </summary>
+    public class ServerEntryPoint : IServerEntryPoint
+    {
+        private readonly ILibraryManager _libraryManager;
+        private readonly ILibraryMonitor _libraryMonitor;
+        private readonly ILogger _logger;
+        private readonly IServerApplicationHost _appHost;
+        private readonly IFileSystem _fileSystem;
+        private readonly IHttpServer _httpServer;
+        private readonly IServerConfigurationManager _configurationManager;
+        private readonly ILocalizationManager _localizationManager;
+        private readonly IServerManager _serverManager;
+        private readonly IProviderManager _providerManager;
+        private readonly IFileOrganizationService _fileOrganizationService;
+
+        private MovieOrganizerApi _api;
+        private MovieOrganizerService _service;
+
+        public static ServerEntryPoint Instance { get; private set; }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="jsonSerializer"></param>
+        /// <param name="sessionManager"> </param>
+        /// <param name="userDataManager"></param>
+        /// <param name="libraryManager"> </param>
+        /// <param name="logger"></param>
+        /// <param name="httpClient"></param>
+        /// <param name="appHost"></param>
+        /// <param name="fileSystem"></param>
+        public ServerEntryPoint(
+            IServerConfigurationManager configurationManager, 
+            ILibraryManager libraryManager,
+            ILibraryMonitor libraryMonitor, 
+            ILogManager logger, 
+            IServerApplicationHost appHost, 
+            IHttpServer httpServer, 
+            IFileSystem fileSystem,
+            ILocalizationManager localizationManager,
+            IServerManager serverManager,
+            IProviderManager providerManager,
+            IFileOrganizationService fileOrganizationService)
+        {
+            Instance = this;
+            _libraryManager = libraryManager;
+            _libraryMonitor = libraryMonitor;
+            _logger = logger.GetLogger("MovieOrganizer");
+            _appHost = appHost;
+            _fileSystem = fileSystem;
+            _configurationManager = configurationManager;
+            _httpServer = httpServer;
+            _localizationManager = localizationManager;
+            _serverManager = serverManager;
+            _providerManager = providerManager;
+            _fileOrganizationService = fileOrganizationService;
+
+            var serviceStackHost = (IAppHost)httpServer;
+            serviceStackHost.RawHttpHandlers.Add(ProcessRequestRaw);
+
+            _service = new MovieOrganizerService(
+                _configurationManager, 
+                logger, 
+                _fileSystem, 
+                _appHost,
+                _libraryMonitor,
+                _libraryManager,
+                _localizationManager,
+                _serverManager,
+                _providerManager,
+                _fileOrganizationService);
+
+            _api = new MovieOrganizerApi(logger, _service, _libraryManager);
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Run()
+        {
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+        }
+
+        public virtual HttpAsyncTaskHandler ProcessRequestRaw(IHttpRequest request)
+        {
+            MemoryStream resultStream = null;
+
+            if (request.PathInfo.Contains("/components/fileorganizer/fileorganizer.js"))
+            {
+                resultStream = HtmlHelper.OrganizerScript;
+            }
+            else if (request.PathInfo.Contains("/components/fileorganizer/fileorganizer.template.html"))
+            {
+                resultStream = HtmlHelper.OrganizerTemplate;
+            }
+
+            if (resultStream != null)
+            {
+                var handler = new CustomActionHandler((httpReq, httpRes) =>
+                {
+                    httpRes.ContentType = "text/html";
+
+                    lock (resultStream)
+                    {
+                        resultStream.Seek(0, SeekOrigin.Begin);
+                        resultStream.WriteTo(httpRes.OutputStream);
+                        httpRes.EndRequest();
+                    }
+
+                    httpRes.End();
+                });
+
+                return handler;
+            }
+
+            return null;
+        }
+    }
+}
