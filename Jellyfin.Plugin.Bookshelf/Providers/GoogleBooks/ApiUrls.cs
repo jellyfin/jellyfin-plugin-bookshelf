e80f3af818d5b40884e81e344a9959e211b0c5c6
Anthony Lavado <anthony@lavado.ca>
2020-02-06 00:34:32 -0500

    Remove API Key from Google Books requests
    As per the API docs, searching and retrieving volume data does need
    auth, but not an API Key.
    
    Reference: https://developers.google.com/books/docs/v1/using

--- Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/ApiUrls.cs
+++ Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/ApiUrls.cs
@@ -4,7 +4,7 @@
     {
         private const string ApiKey = "AIzaSyCFqC6-BAZwqvOBnbNYN8fbK-R1swtnDac";
         // GoogleBooks API Endpoints
-        public const string SearchUrl = @"https://www.googleapis.com/books/v1/volumes?q={0}&startIndex={1}&maxResults={2}&key=" + ApiKey;
-        public const string DetailsUrl = @"https://www.googleapis.com/books/v1/volumes/{0}?key=" + ApiKey;
+        public const string SearchUrl = @"https://www.googleapis.com/books/v1/volumes?q={0}&startIndex={1}&maxResults={2}";
+        public const string DetailsUrl = @"https://www.googleapis.com/books/v1/volumes/{0}";
     }
 }
