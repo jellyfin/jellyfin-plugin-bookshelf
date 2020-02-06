01a929deaef92238b91e2f92ca1d9fba3b702130
Anthony Lavado <anthony@lavado.ca>
2020-02-06 00:35:49 -0500

    Remove API key fully, bump version

--- Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/ApiUrls.cs
+++ Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/ApiUrls.cs
@@ -2,7 +2,6 @@
 {
     public static class GoogleApiUrls
     {
-        private const string ApiKey = "AIzaSyCFqC6-BAZwqvOBnbNYN8fbK-R1swtnDac";
         // GoogleBooks API Endpoints
         public const string SearchUrl = @"https://www.googleapis.com/books/v1/volumes?q={0}&startIndex={1}&maxResults={2}";
         public const string DetailsUrl = @"https://www.googleapis.com/books/v1/volumes/{0}";
