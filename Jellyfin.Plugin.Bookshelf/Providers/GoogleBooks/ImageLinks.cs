67a4e59a29b1a4a9bb6948f5695e808e5939117d
Pithaya <19533412+Pithaya@users.noreply.github.com>
2023-09-23 17:54:38 +0200

    Update property comments to use the Google Books API's descriptions

--- Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/ImageLinks.cs
+++ Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/ImageLinks.cs
@@ -1,4 +1,4 @@
-using System.Text.Json.Serialization;
+﻿using System.Text.Json.Serialization;
 
 namespace Jellyfin.Plugin.Bookshelf.Providers.GoogleBooks
 {
@@ -8,40 +8,40 @@ namespace Jellyfin.Plugin.Bookshelf.Providers.GoogleBooks
     public class ImageLinks
     {
         /// <summary>
-        /// Gets or sets the small thumbnail.
+        /// Gets or sets the image link for small thumbnail size (width of ~80 pixels).
         /// </summary>
         /// <remarks>
-        /// // Only the 2 thumbnail images are available during the initial search.
+        /// Only the 2 thumbnail images are available during the initial search.
         /// </remarks>
         [JsonPropertyName("smallThumbnail")]
         public string? SmallThumbnail { get; set; }
 
         /// <summary>
-        /// Gets or sets the thumbnail.
+        /// Gets or sets the image link for thumbnail size (width of ~128 pixels).
         /// </summary>
         [JsonPropertyName("thumbnail")]
         public string? Thumbnail { get; set; }
 
         /// <summary>
-        /// Gets or sets the small image.
+        /// Gets or sets the image link for small size (width of ~300 pixels).
         /// </summary>
         [JsonPropertyName("small")]
         public string? Small { get; set; }
 
         /// <summary>
-        /// Gets or sets the medium image.
+        /// Gets or sets the image link for medium size (width of ~575 pixels).
         /// </summary>
         [JsonPropertyName("medium")]
         public string? Medium { get; set; }
 
         /// <summary>
-        /// Gets or sets the large image.
+        /// Gets or sets the image link for large size (width of ~800 pixels).
         /// </summary>
         [JsonPropertyName("large")]
         public string? Large { get; set; }
 
         /// <summary>
-        /// Gets or sets the extra large image.
+        /// Gets or sets the image link for extra large size (width of ~1280 pixels).
         /// </summary>
         [JsonPropertyName("extraLarge")]
         public string? ExtraLarge { get; set; }
--- Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/VolumeInfo.cs
+++ Jellyfin.Plugin.Bookshelf/Providers/GoogleBooks/VolumeInfo.cs
@@ -10,61 +10,64 @@ namespace Jellyfin.Plugin.Bookshelf.Providers.GoogleBooks
     public class VolumeInfo
     {
         /// <summary>
-        /// Gets or sets the title.
+        /// Gets or sets the volume title.
         /// </summary>
         [JsonPropertyName("title")]
         public string? Title { get; set; }
 
         /// <summary>
-        /// Gets or sets the list of authors.
+        /// Gets or sets the names of the authors and/or editors for this volume.
         /// </summary>
         [JsonPropertyName("authors")]
         public IReadOnlyList<string> Authors { get; set; } = Array.Empty<string>();
 
         /// <summary>
-        /// Gets or sets the published date.
+        /// Gets or sets the date of publication.
         /// </summary>
         [JsonPropertyName("publishedDate")]
         public string? PublishedDate { get; set; }
 
         /// <summary>
-        /// Gets or sets the image links.
+        /// Gets or sets a list of image links for all the sizes that are available.
         /// </summary>
         [JsonPropertyName("imageLinks")]
         public ImageLinks? ImageLinks { get; set; }
 
         /// <summary>
-        /// Gets or sets the publisher.
+        /// Gets or sets the publisher of this volume.
         /// </summary>
         [JsonPropertyName("publisher")]
         public string? Publisher { get; set; }
 
         /// <summary>
-        /// Gets or sets the description.
+        /// Gets or sets the synopsis of the volume.
+        /// The text of the description is formatted in HTML and includes simple formatting elements, such as b, i, and br tags.
         /// </summary>
         [JsonPropertyName("description")]
         public string? Description { get; set; }
 
         /// <summary>
-        /// Gets or sets the main category.
+        /// Gets or sets the main category to which this volume belongs.
+        /// It will be the category from the categories list that has the highest weight.
         /// </summary>
         [JsonPropertyName("mainCategory")]
         public string? MainCategory { get; set; }
 
         /// <summary>
-        /// Gets or sets the list of categories.
+        /// Gets or sets the list of subject categories, such as "Fiction", "Suspense", etc.
         /// </summary>
         [JsonPropertyName("categories")]
         public IReadOnlyList<string> Categories { get; set; } = Array.Empty<string>();
 
         /// <summary>
-        /// Gets or sets the average rating.
+        /// Gets or sets the mean review rating for this volume. (min = 1.0, max = 5.0).
         /// </summary>
         [JsonPropertyName("averageRating")]
         public float? AverageRating { get; set; }
 
         /// <summary>
-        /// Gets or sets the language.
+        /// Gets or sets the best language for this volume (based on content).
+        /// It is the two-letter ISO 639-1 code such as 'fr', 'en', etc.
         /// </summary>
         [JsonPropertyName("language")]
         public string? Language { get; set; }
